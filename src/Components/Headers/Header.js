548f1627211974c42c6bcd221d5a1e8427857526
Agnik Saha <agniksaha2019@gmail.com>
2021-08-12 08:45:06 +0530

    header&definition

---
+++ src/Components/Headers/Header.js
@@ -0,0 +1,50 @@
+import { TextField, ThemeProvider,MenuItem} from '@material-ui/core'
+import { createTheme } from '@material-ui/core/styles';
+import React from 'react'
+import "./Header.css"
+import categories from '../../data/category';
+
+const Header = ({category,setCategory, word, setWord}) => {
+    const darkTheme = createTheme({
+        palette: {
+            primary: {
+                main:'#fff'
+            },
+          type: 'dark',
+        },
+      });
+
+      const handleChange = (language) => {
+          setCategory(language);
+          setWord("");
+      }
+
+    return (
+        <div className="header">
+            <span className="title">{word? word:"Word-hunt"}</span>
+            <div className="inputs">
+                <ThemeProvider theme={darkTheme}>
+                    <TextField className="search"
+                             label="Search a word"
+                             label="Standard" 
+                             value={word} 
+                             onChange={(e)=>setWord(e.target.value)}/>
+                    <TextField className="select"
+                        select
+                        label="language"
+                        value={category}
+                        onChange={(e) => handleChange(e.target.value)}
+                        >  
+                        {  categories.map((option)=> (
+                                <MenuItem key={option.label} 
+                                            value={option.label}> {option.value}</MenuItem>
+                               )) }                       
+                        </TextField>
+                </ThemeProvider>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
